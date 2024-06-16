b3b77085599c5ddbe34f0c7e93d2993556921282
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2024-06-17 00:11:58 +0330

    Add some static data files

---
+++ src/data/avatars.ts
@@ -0,0 +1,14 @@
+const avatars = [
+  { id: 1, src: '/avatar/1.jpg' },
+  { id: 2, src: '/avatar/2.jpg' },
+  { id: 3, src: '/avatar/3.jpg' },
+  { id: 4, src: '/avatar/4.jpg' },
+  //   { id: 5, src: '/avatar/5.jpg' },
+  { id: 6, src: '/avatar/6.jpg' },
+  { id: 7, src: '/avatar/7.jpg' },
+  { id: 8, src: '/avatar/8.jpg' },
+  //   { id: 9, src: '/avatar/9.jpg' },
+  { id: 10, src: '/avatar/10.jpg' },
+];
+
+export default avatars;
---
+++ src/data/toastInfo.ts
@@ -0,0 +1,49 @@
+const signUpToastInfo = {
+  message: 'New account successfully created',
+  desc: 'Redirecting to profile page ...',
+  success: true,
+};
+
+const loginToastInfo = {
+  message: 'You have successfully logged-in',
+  desc: 'Redirecting to profile page ...',
+  success: true,
+};
+
+const sendEmailToastInfo = {
+  message: 'OTP token sent successfully',
+  desc: 'Redirecting to reset-password page',
+  success: true,
+};
+
+const resetPasswordToastInfo = {
+  message: 'You have successfully updated your password',
+  desc: 'Redirecting to profile page ...',
+  success: true,
+};
+
+const updateProfileToastInfo = {
+  message: 'You have successfully updated your profile',
+  desc: 'Redirecting to profile profile ...',
+  success: true,
+};
+
+const logoutToastInfo = {
+  message: 'You have successfully logged-out',
+  desc: 'Redirecting you to login page ...',
+};
+
+const deleteAccountToastInfo = {
+  message: 'You have successfully deleted your account',
+  desc: 'Redirecting you to sign-up page ...',
+};
+
+export {
+  signUpToastInfo,
+  loginToastInfo,
+  sendEmailToastInfo,
+  resetPasswordToastInfo,
+  updateProfileToastInfo,
+  logoutToastInfo,
+  deleteAccountToastInfo,
+};
